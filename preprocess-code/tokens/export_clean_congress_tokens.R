d6e3ca0a671e46ebee8a03a21c0f48b09343cc68
Steph Buongiorno <52008049+stephbuon@users.noreply.github.com>
2022-08-29 15:27:34 -0500

    Update export_clean_congress_tokens.R

--- preprocess-code/tokens/export_clean_congress_tokens.R
+++ preprocess-code/tokens/export_clean_congress_tokens.R
@@ -15,9 +15,7 @@ registerDoParallel(cl)
 
 import_stopwords_as_regex <- function() {
   
-  #stopwords <- read_csv("/scratch/group/pract-txt-mine/sbuongiorno/stopwords_text2vec_1017something.csv") 
-  stopwords <- read_csv("/home/stephbuon/projects/congress-shiny/preprocess-data/stopwords_text2vec.csv")
-  
+  stopwords <- read_csv("/scratch/group/pract-txt-mine/sbuongiorno/congress-shiny/preprocess-data/stopwords_text2vec.csv") 
   
   stopwords <- stopwords %>%
     summarise(all = paste0(stop_word, collapse="|"))
